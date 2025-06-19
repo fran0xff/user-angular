2beef9c98f70d6279ee857e38702c68b4da305f5
fran <franciscojavierarevalo@hotmail.com>
2025-06-19 20:45:49 +0200

    Agregar formulario de usuario y mejorar la visualización en el componente UserApp

--- src/app/components/user-app.component.ts
+++ src/app/components/user-app.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import { UserService } from '../services/user.service';
 import { UserComponent } from './user/user.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @Component({
   selector: 'user-app',
-  imports: [UserComponent],
+  imports: [UserComponent, UserFormComponent],
   templateUrl: './user-app.component.html',
 })
 export class UserAppComponent implements OnInit {
@@ -22,6 +23,7 @@ export class UserAppComponent implements OnInit {
       
   }
 
-
-
+  addUser(user: User) {
+    this.users = [... this.users, {... user, id: new Date().getTime()}];
+  }
 }
--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { User } from '../../models/user';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-user-form',
-  imports: [],
+  selector: 'user-form',
+  imports: [FormsModule],
   templateUrl: './user-form.component.html',
 })
 export class UserFormComponent {
 
+  user: User;
+
+  @Output() newUserEventEmitter: EventEmitter<User> = new EventEmitter();
+
+  constructor() {
+    this.user = new User();
+  }
+
+  onSubmit(userForm: NgForm) {
+    if (userForm.valid) {
+      this.newUserEventEmitter.emit(this.user);
+      console.log(this.user); 
+    }
+    userForm.reset();
+    userForm.resetForm();
+    
+  }
 }
